a5d9ce15438d6ddc533d7c474ca5352f3b34b802
melinaballay <melinasuper256@gmail.com>
2024-01-05 01:02:52 -0300

    Agregado nuevo método a la clase Animal

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var Animal = /** @class */ (function () {
     Animal.prototype.sonido = function () {
         return 'hace un sonido chistoso';
     };
+    Animal.prototype.nuevoMetodo = function () {
+        return 'esto es un nuevo metodo creado';
+    };
     return Animal;
 }());
 var animal = new Animal('chufo y simon');
 console.log("mi animal se llama ".concat(animal.nombre));
 console.log("mi animal  ".concat(animal.sonido()));
+console.log("mi animal tiene un ".concat(animal.nuevoMetodo()));
--- index.ts
+++ index.ts
@@ -5,7 +5,11 @@ sonido(){
     return 'hace un sonido chistoso';
 }
 
+nuevoMetodo(){
+return 'esto es un nuevo metodo creado'
+}
 }
 const animal = new Animal('chufo y simon');
 console.log(`mi animal se llama ${animal.nombre}`);
 console.log(`mi animal  ${animal.sonido()}`);
+console.log(`mi animal tiene un ${animal.nuevoMetodo()}`);
